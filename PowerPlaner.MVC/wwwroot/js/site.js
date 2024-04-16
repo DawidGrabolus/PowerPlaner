7bb01dcc546987c881d864af6413638ba1ce7170
DawidGrabolus <90209663+DawidGrabolus@users.noreply.github.com>
2024-04-17 01:39:16 +0200

    First commit

---
+++ PowerPlaner.MVC/wwwroot/js/site.js
@@ -0,0 +1,22 @@
+﻿
+$(document).ready(function () {
+    //NAVBAR
+    $('#toggleSidebar').click(function () {
+        $('.left-bar').toggleClass('collapsed');
+        $(".list-group").slideUp(200);
+    });
+    $(".plans").click(function () {
+        if (!$('.left-bar').hasClass('collapsed')) {
+            $(".list-group").slideToggle("slow");
+        }
+
+    });
+
+    $("#v-pills-home-tab, #v-pills-profile-tab, #v-pills-settings-tab").click(function () {
+        $(".list-group").slideUp("slow");
+    });
+
+    //Link
+   
+}
+    
