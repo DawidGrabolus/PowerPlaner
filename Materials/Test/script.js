2695a50bef3ec1684a79e3a0a2caf185760284eb
DawidGrabolus <90209663+DawidGrabolus@users.noreply.github.com>
2024-04-17 01:40:19 +0200

    Materials

---
+++ Materials/Test/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    $('#toggleSidebar').click(function () {
+        $('.left-bar').toggleClass('collapsed');
+        $(".list-group").slideUp(200);
+    });
+    $(".plans").click(function () {
+        if (!$('.left-bar').hasClass('collapsed')) {
+            $(".list-group").slideToggle("slow");
+        }
+
+    });
+
+    $("#v-pills-home-tab, #v-pills-profile-tab, #v-pills-settings-tab").click(function () {
+        $(".list-group").slideUp("slow");
+    });
+});
